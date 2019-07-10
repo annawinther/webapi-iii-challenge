9516a22165136241fb3df3630268e9e74fc6116a
Anna W <anski.anna@gmail.com>
2019-07-10 12:39:57 +0100

    get bost by id working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -12,8 +12,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const post = await postDb.getById(id)
+        if (post) {
+            res.status(200).json(post);
+          } else {
+            res.status(404).json({ message: 'post with that is not found ' });
+          }
+    } catch (error){
+        res.status(500).json({ message: "error getting post with that id"})
+    }
 });
 
 router.delete('/:id', (req, res) => {
