4b965a645ca3e164e6b4c29ad68777ffe014bc0a
Anna W <anski.anna@gmail.com>
2019-07-10 14:33:48 +0100

    logger middleware build (missig the timestamp)

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const server = express();
 
 server.use(express.json());
 
+server.use(logger);
 // server.use('/api/posts', postsRouter);
 server.use('/api/users', userRouter);
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(`${req.method} Request, ${req.url}`);
+  next();
 
 };
 
