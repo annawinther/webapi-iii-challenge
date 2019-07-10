9cabf33ccb3a0fb6f8f47b48c906314fe920f30e
Anna W <anski.anna@gmail.com>
2019-07-10 13:16:09 +0100

    update user working

--- users/userRouter.js
+++ users/userRouter.js
@@ -63,7 +63,20 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-
+    const userData = req.body;
+    const id = req.params.id
+    try {
+        const user = await userDb.update(id, userData);
+        if(user) {
+            res.status(200).json(userData)
+        } else {
+            res.status(404).json({ message: 'The hub could not be found' });
+        }
+    } catch(error) {
+        res.status(500).json({
+            message: 'Error updating the hub',
+          });
+    }
 });
 
 //custom middleware
