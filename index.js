aedf8b4b8d20e3c2f35fa48d4a96be919b5d89cf
Anna W <anski.anna@gmail.com>
2019-07-11 12:13:53 +0100

    changed port to use env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server');
 
-server.listen(4001, () => {
-    console.log('\n* Server Running on http://localhost:4001 *\n');
+const port = process.env.PORT || 4001;
+
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`);
 })
